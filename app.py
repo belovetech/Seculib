17f19a6efcd90292c929e0ce55e0fcc7d2d10ee9
Abeeb Raheem <belovetech@gmail.com>
2024-07-27 13:38:38 +0100

    (chore): persist the user data in the database

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ from dotenv import load_dotenv
 import jwt
 import os
 import datetime
+from user_manager import UserManager
 
 
 app = Flask(__name__)
@@ -11,7 +12,9 @@ app = Flask(__name__)
 load_dotenv()
 SECRET_KEY = os.getenv('SECRET_KEY')
 
-users = {}
+# users = {}
+
+user_manager = UserManager('users.db')
 
 
 @app.route('/healthz', methods=['GET'])
@@ -41,32 +44,88 @@ def unprotected():
 @app.route('/register', methods=['POST'])
 def register():
     data = request.get_json()
-    username = data['username']
+
+    if not data:
+        return jsonify({'message': 'Missing data!'}), 400
+
+    if 'full_name' not in data:
+        return jsonify({'message': 'full_name is required'}), 400
+
+    if 'matric_no' not in data:
+        return jsonify({'message': 'matric_no is required'}), 400
+
+    if 'password' not in data:
+        return jsonify({'message': 'password is required'}), 400
+
+    full_name = data['full_name']
+    matric_no = data['matric_no']
     password = generate_password_hash(data['password'])
 
-    if username in users:
+    user_with_matric_no_exists = user_manager.get_user_by_matric_no(matric_no)
+
+    if user_with_matric_no_exists:
         return jsonify({'message': 'User already exists!'}), 400
 
-    users[username] = password
+    user_manager.create_user(full_name, matric_no, password)
 
-    print(users)
     return jsonify({'message': 'Registered successfully!'}), 201
 
 @app.route('/login', methods=['POST'])
 def login():
-    data = request.get_json()
-    username = data['username']
-    password = data['password']
+    try:
+        data = request.get_json()
 
-    if username not in users or not check_password_hash(users[username], password):
-        return jsonify({'message': 'Invalid credentials!'}), 401
+        if not data:
+            return jsonify({'message': 'Missing data!'}), 400
 
-    token = jwt.encode({
-        'user': username,
-        'exp': datetime.datetime.now() + datetime.timedelta(hours=1)
-    }, SECRET_KEY)
+        if 'matric_no' not in data:
+            return jsonify({'message': 'matric_no is required'}), 400
 
-    return jsonify({'token': token})
+        if 'password' not in data:
+            return jsonify({'message': 'password is required'}), 400
+
+        matric_no = data['matric_no']
+        password = data['password']
+
+        user = user_manager.get_user_by_matric_no(matric_no)
+
+        if not user or not check_password_hash(user['password'], password):
+            return jsonify({'message': 'Invalid credentials!'}), 401
+
+        token = jwt.encode({
+            'user': matric_no,
+            'exp': datetime.datetime.now() + datetime.timedelta(hours=1)
+        }, SECRET_KEY)
+
+        return jsonify({'message': 'Logged in!', 'data': {'token':token}}), 200
+    except Exception as e:
+        print(e)
+        return jsonify({'message': 'An error occurred!'}), 500
+
+
+
+#get user profile
+@app.route('/profile', methods=['GET'])
+def profile():
+    try:
+        bearerToken = request.headers.get('Authorization')
+        if not bearerToken:
+            return jsonify({'message': 'Token is missing!'}), 401
+
+        try:
+            token = bearerToken.split(' ')[1]
+            data = jwt.decode(token, SECRET_KEY, algorithms=['HS256'])
+        except jwt.PyJWTError as e:
+            print(e)
+            return jsonify({'message': 'Token is invalid!'}), 401
+
+        user = user_manager.get_user_by_matric_no(data['user'])
+        del user['password'] # Remove password from the response
+
+        return jsonify({'data': user, "message": "User profile"}), 200
+    except Exception as e:
+        print(e)
+        return jsonify({'message': 'An error occurred!'}), 500
 
 if __name__ == '__main__':
     app.run(debug=True)
