eb816e6b68890f19703f0f1af94d3de67c6af1a7
Abeeb Raheem <belovetech@gmail.com>
2024-08-05 17:14:56 +0100

    (chore): use redis url for redis connection

--- helpers/redis_client.py
+++ helpers/redis_client.py
@@ -1,8 +1,6 @@
 import os
 import redis
 
-redis_host = os.getenv('REDIS_HOST', 'redis')
-redis_port = int(os.getenv('REDIS_PORT', 6379))
-redis_username = os.getenv('REDIS_USERNAME')
-redis_password = os.getenv('REDIS_PASSWORD')
-redis_client = redis.StrictRedis(host=redis_host, port=redis_port, username=redis_username, password=redis_password, decode_responses=True)
+# use redis url
+redis_url = os.getenv('REDIS_URL')
+redis_client = redis.StrictRedis.from_url(redis_url, decode_responses=True)
