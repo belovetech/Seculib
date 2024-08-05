7d8ea528c829ec4eb1547a4d0c1be5f698f75bdd
Abeeb Raheem <belovetech@gmail.com>
2024-08-05 16:37:24 +0100

    (chore): update redis client connection

--- helpers/redis_client.py
+++ helpers/redis_client.py
@@ -3,4 +3,6 @@ import redis
 
 redis_host = os.getenv('REDIS_HOST', 'redis')
 redis_port = int(os.getenv('REDIS_PORT', 6379))
-redis_client = redis.StrictRedis(host=redis_host, port=redis_port, decode_responses=True)
+redis_username = os.getenv('REDIS_USERNAME')
+redis_password = os.getenv('REDIS_PASSWORD')
+redis_client = redis.StrictRedis(host=redis_host, port=redis_port, username=redis_username, password=redis_password, decode_responses=True)
