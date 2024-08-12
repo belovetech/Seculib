ae8cfa84c0dde834db34ed387658bfa8483bc390
Abeeb Raheem <belovetech@gmail.com>
2024-08-12 20:42:12 +0100

    (feat): move backend implementation to the backend directory

--- api/library/__init__.py
+++
@@ -1,2 +0,0 @@
-#!/usr/bin/env python3
-"""Init module for views"""
--- backend/helpers/generate_traffic.py
+++ backend/helpers/generate_traffic.py
@@ -2,7 +2,7 @@ import asyncio
 import aiohttp
 import time
 
-url = "https://a9de-102-88-69-214.ngrok-free.app/unprotected"
+url = "https://secure-auth-dos-prevention.onrender.com/api/v1/books"
 num_requests = 10000  # Total number of requests to send
 concurrent_requests = 50  # Number of concurrent requests
 
