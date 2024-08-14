55eb4b515f162c88c963e33b43b93bae14fcbcb2
Abeeb Raheem <belovetech@gmail.com>
2024-08-14 01:13:09 +0100

    (chore): fix navbar

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,11 +1,11 @@
 <script lang="ts">
 import { defineComponent } from 'vue'
-import NavBar from './components/NavBar.vue'
+import Navbar from './components/NavBar.vue'
 
 export default defineComponent({
   name: 'App',
   components: {
-    NavBar
+    Navbar
   }
 })
 </script>
